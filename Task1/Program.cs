1addacfa55b9c3b5ee49b55b5ba3f59ff3e55809
unknown <selmarionx@gmail.com>
2022-10-04 22:14:52 +0300

    task1 OK

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -3,4 +3,9 @@
 // 782 -> 8
 // 918 -> 1
 
-
+Console.Clear();
+Console.WriteLine("программа, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. ");
+Console.Write("Введи трёхзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
