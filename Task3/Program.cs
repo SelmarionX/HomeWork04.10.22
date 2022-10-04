ef08d8f1723fd6233d735185048271db6fb60a8a
unknown <selmarionx@gmail.com>
2022-10-04 22:33:29 +0300

    task3 OK

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -3,3 +3,21 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+Console.Clear();
+Console.WriteLine("Программа,которая принимает на вход цифру, обозначающую день недели,и проверяет,является ли этот день выходным.");
+
+Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber) {
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("этот день выходной -> да");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("Вы ввели неверное значение дня недели");
+  }
+  else Console.WriteLine("этот день не выходной -> нет");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
